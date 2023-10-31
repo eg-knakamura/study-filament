24f75c9774cc319ad4d3f52106fa48ce30556dbd
kenji nakamura <k-nakamura@egstock.co.jp>
2023-10-31 13:05:38 +0900

    update readme&admin

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ git clone git@github.com:eg-knakamura/study-filament.git
 cd study-filament
 make init
 ```
+
+## 管理画面ログイン
+```
+URL: http://localhost/admin/login
+メールアドレス：admin@example.com
+パスワード：aaaaaaaa
+```
--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-
     /**
      * Auto generated seed file
      *
@@ -14,24 +13,20 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        
+        \DB::table("users")->delete();
 
-        \DB::table('users')->delete();
-        
-        \DB::table('users')->insert(array (
-            0 => 
-            array (
-                'created_at' => '2023-10-20 02:16:04',
-                'email' => 'admin@egstock.co.jp',
-                'email_verified_at' => NULL,
-                'id' => 1,
-                'name' => 'admin',
-                'password' => '$2y$10$JSVj95aCIMPJqZJXDiHzDe9YZP8Sj9MI6uW15WCm5q0/gjT.dU7ca',
-                'remember_token' => NULL,
-                'updated_at' => '2023-10-20 02:16:04',
-            ),
-        ));
-        
-        
+        \DB::table("users")->insert([
+            0 => [
+                "created_at" => "2023-10-20 02:16:04",
+                "email" => "admin@example.com",
+                "email_verified_at" => null,
+                "id" => 1,
+                "name" => "admin",
+                "password" =>
+                    '$2y$10$JSVj95aCIMPJqZJXDiHzDe9YZP8Sj9MI6uW15WCm5q0/gjT.dU7ca',
+                "remember_token" => null,
+                "updated_at" => "2023-10-20 02:16:04",
+            ],
+        ]);
     }
 }
